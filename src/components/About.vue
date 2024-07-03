dd764ebb2edc37a400e0fbffd7bd20a1bdb281b3
lebomtomboti <lebomtomboti17@gmail.com>
2024-07-03 16:17:12 +0200

    added

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,16 +1,33 @@
 <template>
-    <div>
-      <h1>About Page</h1>
-      <!-- Your home page content here -->
+  <div class="container pb-5">
+    <h1 class="mb-0 text-danger p-5">Profile</h1>
+    <div class="col d-block d-md-flex">
+      <img src="https://i.ibb.co/dg3pmsj/AGXqz-Dm1-PCZQku-Zfro9-Khe2-AXFk-Pxc-I6od-H0j8-JVE6-Hydsx-BGNo-Fmon-R2-Pb-N-5el-Dv-Mv-Xk1-f-N1-IUPZV.png" alt="Google Drive Image" class="home1 mb-5 img-fluid rounded-bottom-circle bg-white" loading="lazy" /> 
+      <div class="abts text-start d-block">
+        <h3 class="text-white mt-3"><span class="text-danger">Full Name</span>: Lebu Mtomboti</h3>
+        <h3 class="text-white mt-3"><span class="text-danger">Date Of Birth</span>: 04 March 2002</h3>
+        <h3 class="text-white mt-3"><span class="text-danger">Gender</span>: Female</h3>
+        <h3 class="text-white mt-3"><span class="text-danger">Language</span>: English & Xhosa</h3>
+        <h3 class="text-white mt-3"><span class="text-danger">Hobbies</span>: Reading Books</h3>
+        <h3 class="text-white mt-3"><span class="text-danger">Cell Number</span>: 0833509812</h3>
+        <h3 class="text-white mt-3"><span class="text-danger">Email</span>: lebomtomboti17@gmail.com</h3>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'About'
+    <h1 class="mb-0 text-danger p-5">About Me</h1>
+    <div class="text-white">
+      <p v-for="paragraph in aboutParagraphs" :key="paragraph">{{ paragraph }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    aboutParagraphs() {
+      return this.$store.state.about.paragraphs; 
+    }
+  },
+  mounted() {
+    this.$store.dispatch("fetchAbout");
   }
-  </script>
-  
-  <style scoped>
-  /* Your component-specific styles here */
-  </style>
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,32 @@
-import { createStore } from 'vuex'
+import Vuex from 'vuex';
+import Vue from 'vue';
+Vue.use(Vuex);
 
-export default createStore({
+export default new Vuex.Store({
   state: {
-  },
-  getters: {
+    about: {
+      paragraphs: [] 
+    }
   },
   mutations: {
+    SET_ABOUT(state, aboutData) {
+      state.about.paragraphs = aboutData; 
+    }
   },
   actions: {
-  },
-  modules: {
+    async fetchAbout({ commit }) {
+      try {
+        
+        const response = await fetch('https://api.example.com/about');
+        if (!response.ok) {
+          throw new Error('Failed to fetch about data');
+        }
+        const aboutData = await response.json();
+        commit('SET_ABOUT', aboutData); 
+      } catch (error) {
+        console.error('Error fetching about data:', error);
+      }
+    }
   }
-})
+});
+
