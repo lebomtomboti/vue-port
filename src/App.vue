9dc58b5179f6dedcba51a8978e01c73fe8ae0c7e
lebomtomboti <lebomtomboti17@gmail.com>
2024-07-04 16:13:13 +0200

    added

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
   <div id="app">
+    <Navbar />
     <AboutPage />
+    <ResumePage />
+    <ProjectsPage />
+    <Spinner />
+    <TestimonialsPage />
+    <ContactForm />
+    <footer />
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue';
 import AboutPage from './components/AboutPage.vue';
+import ContactForm from './components/ContactForm.vue';
+import ProjectsPage from './components/ProjectsPage.vue';
+import ResumePage from './components/ResumePage.vue';
+import TestimonialsPage from './components/TestimonialsPage.vue';
+import Footer from './components/Footer.vue';
+import Spinner from './components/Spinner.vue';
 
 
 export default {
   components: {
-    AboutPage
+    // eslint-disable-next-line vue/no-unused-components
+    AboutPage,Navbar,ContactForm,ProjectsPage,ResumePage,TestimonialsPage,Footer,Spinner,
   }
 };
 </script>
