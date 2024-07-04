d9902a51d0f3132fc8088579409916bffc71b6d5
lebomtomboti <lebomtomboti17@gmail.com>
2024-07-04 15:46:59 +0200

    added

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,6 +1,6 @@
-<template>
+<template lang="">
   <div>
-    about page
+    
   </div>
 </template>
 <script>
@@ -8,6 +8,6 @@ export default {
   
 }
 </script>
-<style>
+<style lang="">
   
 </style>
